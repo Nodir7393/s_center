ee8da48f72b058be0e2e148aa50e83cb9ba75c52
Nodir7393 <nodirbek7393@gmail.com>
2025-09-18 18:15:35 +0500

    Nodir

--- api/config/sanctum.php
+++ api/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'dev.pcbuild.uz,localhost,localhost:5173,127.0.0.1,127.0.0.1:8000,localhost:8000,::1',
+        'dev.pcbuild.uz,localhost,localhost:5173,127.0.0.1,127.0.0.1:8000,apidev.pcbuild.uz,localhost:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
